bcb6371116f9e231d21526b6d34c2404e3d22ce3
João Castro <joaofbomfimc@gmail.com>
2023-10-15 19:20:12 -0300

    :sparkles: Mudando a home

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-5 flex justify-between bg-purple-900 text-zinc-100">
+      <h1 className="text-5xl font-bold">
+        NetFluxo
+      </h1>
+    </div>
+  )
+}
